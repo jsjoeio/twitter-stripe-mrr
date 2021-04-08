b3d6663d270327f88899e5d1d14b706dc81d1640
Joe Previte <jjprevite@gmail.com>
2021-04-08 08:12:14 -0700

    refactor: move kFormatter

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 })
 const iconsTypes = {
-  'square': { green: '🟩', yellow: '🟨', gray: '⬜', },
-  'circle': { green: '🟢', yellow: '🟡', gray: '⚪', }
+  square: { green: "🟩", yellow: "🟨", gray: "⬜" },
+  circle: { green: "🟢", yellow: "🟡", gray: "⚪" },
 }
+
+const EXPECTED_ENV_VARS = [
+  "TWITTER_CONSUMER_KEY",
+  "TWITTER_CONSUMER_KEY",
+  "TWITTER_ACCESS_TOKEN_KEY",
+  "TWITTER_ACCESS_TOKEN_SECRET",
+  "STRIPE_API_KEY",
+]
 
 async function getStripeRevenue(startRangeTimestamp, endRangeTimestamp) {
   // credit here: https://stackoverflow.com/a/53775391/3015595
@@ -27,7 +35,9 @@ async function getStripeRevenue(startRangeTimestamp, endRangeTimestamp) {
   })
 
   // payouts returned by Stripe API are in cents, so we divide by 100
-  const payouts = payoutsInCents.data.map(function(x) { return x.amount / 100 } )
+  const payouts = payoutsInCents.data.map(function (x) {
+    return x.amount / 100
+  })
 
   const amountTotal = payouts.reduce((a, b) => ({
     amount: a + b,
@@ -42,7 +52,7 @@ async function getStripeRevenue(startRangeTimestamp, endRangeTimestamp) {
  * @example there are ten squares total, and n is 5, then it should return
  * "MRR: 0 🟩🟩🟩🟩🟩🟨⬜⬜⬜⬜  5K" or "MRR: 0 🟢🟢🟢🟢🟢🟡⚪⚪⚪⚪  5K"
  */
-function buildMRRSquaresForTwitter(n, goal, icon = 'square') {
+function buildMRRSquaresForTwitter(n, goal, icon = "square") {
   const GOAL_AS_K = kFormatter(goal)
   let SQUARES = ""
   let count = 0
@@ -70,14 +80,7 @@ function buildMRRSquaresForTwitter(n, goal, icon = 'square') {
   return `MRR: 0 ${SQUARES} ${GOAL_AS_K}`
 }
 
-// source: https://stackoverflow.com/a/9461657/3015595
-function kFormatter(num) {
-  return Math.abs(num) > 999
-    ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "K"
-    : Math.sign(num) * Math.abs(num)
-}
-
-;(async function () {
+async function removeMe() {
   const totalRevenueForMonth = await getStripeRevenue(
     startRangeTimestamp,
     endRangeTimestamp
@@ -98,4 +101,58 @@ function kFormatter(num) {
     if (err) throw new Error("Failed to update profile")
     console.log("🎉 Success! Updated Twitter bio/location")
   })
-})()
+}
+
+/**
+ * The main function which starts the script
+ */
+async function main() {
+  try {
+    // Verify that all the environment variables are set
+    const ACTUAL_ENV_VARS = EXPECTED_ENV_VARS.map((envVar) =>
+      getEnvironmentVariable(envVar)
+    )
+
+    // validate Twitter API
+    // validate Stripe API
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
+
+//////////////////////////////
+// Helper Functions
+/////////////////////////////
+
+/**
+ * Grabs the environment variable based off the name
+ * @param {string} name the name of the variable
+ * @returns {string} the variable if it exists or throws an error
+ */
+function getEnvironmentVariable(name) {
+  if (!process.env[name]) {
+    throw new Error(`❌ ERROR: could not find ${name} in your environment.`)
+  }
+
+  console.log(`✅ Found ${name} in environment.`)
+  return process.env[name]
+}
+
+/**
+ * Formats a number to have a K or not
+ * @param {number} num the number to format
+ * @returns {string} the `num` formatted with "K"
+ * @example kFormatter(5000) => 5K
+ * @link https://stackoverflow.com/a/9461657/3015595 for more information
+ */
+function kFormatter(num) {
+  return Math.abs(num) > 999
+    ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "K"
+    : Math.sign(num) * Math.abs(num)
+}
+
+//////////////////////////////
+// Helper Functions (end)
+/////////////////////////////
